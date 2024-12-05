676798ce371806bb1d32c3b2751b9f4f7e166d77
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-12-05 18:24:19 +0200

    preparing_for_brain_wave_project

--- client/src/routing/routes.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  auth: {
-    main: "/auth",
-    login: "/auth/login",
-    register: "auth/register",
-  },
-};
--- client/src/types/FormFieldsTypes.ts
+++
@@ -1,8 +0,0 @@
-interface FormFields {
-  username: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-}
-
-export default FormFields;
