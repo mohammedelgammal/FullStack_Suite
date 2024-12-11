c973bd4decc48654cac387d65b34d65d76f683f5
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-12-11 14:08:35 +0200

    configuring_note_taking_app_structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,5 @@
-import { useEffect, useState } from "react";
-import { Todo } from "./types/types";
-import TodoForm from "src/components/TodoForm";
-import TodoList from "src/components/TodoList";
-
-const App = (): JSX.Element => {
-  // Global state
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>(() => {
-    const lsTodos: string | null = localStorage.getItem("todos");
-    if (!lsTodos) return [];
-    return JSON.parse(lsTodos);
-  });
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
-  return (
-    <>
-      <TodoForm todo={todo} setTodo={setTodo} setTodos={setTodos} />
-      <TodoList todos={todos} setTodos={setTodos} />
-    </>
-  );
+const App = () => {
+  return <h1>Note Taking Application</h1>;
 };
 
 export default App;
