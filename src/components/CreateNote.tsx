cb2a17d5643857be8a1da619f46c36dc9a6cc2ac
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-12-15 16:38:52 +0200

    building_create_new_form_built

--- src/components/CreateNote.tsx
+++ src/components/CreateNote.tsx
@@ -1,7 +1,28 @@
+import Creatable from "react-select/creatable";
+import { OptionsType } from "src/types";
+
+const options: OptionsType = [];
+
 const CrateNote = () => {
-    return ( <h1>
-        Create note
-    </h1> );
-}
- 
+  return (
+    <div>
+      <h1>New Note</h1>
+      <form>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input name="title" type="text" />
+        </div>
+        <div>
+          <label htmlFor="tags">Tags</label>
+          <Creatable name="tags" isMulti options={options} />
+        </div>
+        <div>
+          <label htmlFor="body">Body</label>
+          <textarea name="body" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
 export default CrateNote;
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+type OptionType = { value: string; label: string };
+type OptionsType = Array<OptionType>;
+
+export type { OptionType, OptionsType };
