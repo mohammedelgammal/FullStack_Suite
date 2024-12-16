30f93587a2e2b515f888522cfc72b1f6ff389460
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-12-16 16:03:56 +0200

    styling_form_components

--- src/components/CreateNote.tsx
+++ src/components/CreateNote.tsx
@@ -5,20 +5,29 @@ const options: OptionsType = [];
 
 const CrateNote = () => {
   return (
-    <div>
-      <h1>New Note</h1>
+    <div className="flex flex-col justify-center gap-5">
+      <h1 className="text-slate-700 font-semibold font-serif text-3xl mt-10 mb-5">
+        New Note
+      </h1>
       <form>
-        <div>
-          <label htmlFor="title">Title</label>
-          <input name="title" type="text" />
+        <div className="flex items-center mb-5 gap-5">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="title">Title</label>
+            <input className="p-1" name="title" type="text" />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="tags">Tags</label>
+            <Creatable
+              className="min-w-96"
+              name="tags"
+              isMulti
+              options={options}
+            />
+          </div>
         </div>
-        <div>
-          <label htmlFor="tags">Tags</label>
-          <Creatable name="tags" isMulti options={options} />
-        </div>
-        <div>
+        <div className="flex flex-col gap-2">
           <label htmlFor="body">Body</label>
-          <textarea name="body" />
+          <textarea rows={10} name="body" />
         </div>
       </form>
     </div>
