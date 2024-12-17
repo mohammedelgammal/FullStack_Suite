91cd7450de87c17b021e69565511eb986214191b
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-12-17 19:31:58 +0200

    implementing_create_form_functionality

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,9 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
     tasks = Task.objects.all()
-    context = {"tasks": tasks}
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid:
+            form.save()
+        redirect("/")
+
+    context = {"tasks": tasks, "TaskForm": form}
     return render(request, "tasks.html", context)
