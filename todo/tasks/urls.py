940b4de894c2707d6479288ffdd55407ba107cc5
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-12-17 23:23:43 +0200

    implementing_delete_&_update_functionality

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("update-task/<int:pk>", views.update_task, name="update-task"),
+    path("delete-task/<int:pk>", views.delete_task, name="delete-task"),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -11,7 +11,33 @@ def index(request):
         form = TaskForm(request.POST)
         if form.is_valid:
             form.save()
+            form = TaskForm()
         redirect("/")
 
     context = {"tasks": tasks, "TaskForm": form}
     return render(request, "tasks.html", context)
+
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid:
+            form.save()
+            return redirect("/")
+
+    context = {"TaskForm": form}
+    return render(request, "update_task.html", context)
+
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == "POST":
+        task.delete()
+        return redirect("/")
+
+    context = {"task": task}
+    return render(request, "delete_task.html", context)
