cbbaa9bf1818226e9914b57191e84d2e1c298728
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-12-19 01:24:11 +0200

    crm_project_preparation

--- todo/tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index),
-    path("update-task/<int:pk>", views.update_task, name="update-task"),
-    path("delete-task/<int:pk>", views.delete_task, name="delete-task"),
-]
