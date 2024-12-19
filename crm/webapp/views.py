728dd8c5b7992a6644b96258ca9c52dd968bec6c
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-12-19 05:02:49 +0200

    implementing_login_and_logout_functionalities

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterUserForm
+from django.contrib.auth import authenticate, login, logout
+
+from .forms import RegisterUserForm, LoginUserForm
 
 
 def home(request):
@@ -13,10 +15,26 @@ def register(request):
         form = RegisterUserForm(request.POST)
         if form.is_valid:
             form.save()
-            redirect("/")
+            redirect("login")
     context = {"RegisterUserForm": form}
     return render(request, "webapp/register.html", context)
 
 
 def login(request):
-    return render(request, "webapp/my-login.html")
+    form = LoginUserForm()
+    if request.method == "POST":
+        form = LoginUserForm(request, request.POST)
+        if form.is_valid:
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username, password)
+            if user is not None:
+                login(request, user)
+                return redirect("dashboard")
+    context = {"LoginUserForm": form}
+    return render(request, "webapp/my-login.html", context)
+
+
+def logout(request):
+    logout(request)
+    return redirect("login")
