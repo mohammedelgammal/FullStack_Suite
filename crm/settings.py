056e22c9d75de3747c72b3808e341ad92b66b2d9
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2025-01-16 10:01:04 +0200

    updating_db_to_postgres_container

--- crm/settings.py
+++ crm/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = "crm.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5434,
     }
 }
 
