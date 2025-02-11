7b762fc98072641ddd6d8f5dfa05f264cb64b666
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2025-02-11 21:15:04 +0200

    nextapp_starter_boilerplate

--- nextapp/app/(dashboard)/dashboard/users/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-const User = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
-  return <h1>User: {id}</h1>;
-};
-
-export default User;
--- nextapp/app/(root)/about/page.tsx
+++
@@ -1,6 +0,0 @@
-const About = () => {
-  throw Error("error in the about Page");
-  return <h1>About Page</h1>;
-};
-
-export default About;
