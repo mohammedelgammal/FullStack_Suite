4688f590d9e5062f2d26fe8c9bc686b3ddef4dcd
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2025-03-11 11:00:33 +0200

    implementing_products_api_using_api_views_rest

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import status
 
-# Create your views here.
+
+@api_view(["GET", "POST"])
+def get_products(request):
+    if request.method == "GET":
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response({"drinks": serializer.data})
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def get_product(request, id):
+    try:
+        product = Product.objects.get(pk=id)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    elif request.method == "DELETE":
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- py_client/client.py
+++ py_client/client.py
@@ -1,7 +1,7 @@
 import requests
 
-endpoint = "http://localhost:8000/api"
+endpoint = "http://localhost:8000/products"
 
-get_response = requests.get(endpoint, json={"name": "mohammed"}, params={"id": 1})
+response = requests.get(endpoint)
 
-print(get_response.json())
+print(response.json())
