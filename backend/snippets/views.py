55c182617f75ec59ca567a0ee15c13c635bbfaa1
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2025-03-18 07:34:26 +0200

    implementing_generics_classes_views_drf

--- backend/snippets/views.py
+++ backend/snippets/views.py
@@ -1,46 +1,13 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Snippet
 from .serializers import SnippetSerializer
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.http import Http404, HttpResponse
 
 
-class SnippetList(APIView):
-    def get(self):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class SnippetList(ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
 
-class SnippetDetail(APIView):
-    def get_snippet(self, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        snippet = self.get_snippet(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        snippet = self.get_snippet(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        snippet = self.get_snippet(pk)
-        snippet.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+class SnippetDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
