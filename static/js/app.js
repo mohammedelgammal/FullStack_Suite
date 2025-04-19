ba16ebbe08a0b6cf179fd59218ce8072e329865d
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2025-04-19 21:53:48 +0200

    intial_commit

--- static/js/app.js
+++
@@ -1,7 +0,0 @@
-const alertMessages = Array.from(document.getElementsByClassName("alert"));
-
-setTimeout(() => {
-  alertMessages.forEach((message) => {
-    message.style.display = "none";
-  });
-}, 3000);
